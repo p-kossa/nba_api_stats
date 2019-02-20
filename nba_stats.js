9b52d7c66f189e783fcc9a5e4119c40e3631d0b7
Kevin Ossa <kossa526+git@gmail.com>
2019-02-20 15:45:11 -0500

    Added code for getting all current NBA teams and team ids

--- nba_stats.js
+++ nba_stats.js
@@ -1,29 +1,21 @@
 const nba = require('nba.js').default;
 
-var fs = require('fs')
+var fs = require('fs');
 
-nba.data.standings({ date: 'current' }, (err, res) => {
-    if (err) {
-        console.error(err);
-        return;
-    }
-
-    fs.writeFileSync('data.json', JSON.stringify(res, null, 2));
-
-    for (var i = 0; i < res.league.standard.teams.length; i++){
-        console.log(res.league.standard.teams[i].teamId)
-    }
-});
-
-nba.data.teams({ year: '2018' }, (err, res) => {
-    if (err) {
-        console.error(err);
-        return;
-    }
-
+// gets a list of dicts of team names and team ids
+nba.data.teams({ year: '2018' }).then(res => {
     fs.writeFileSync('teams.json', JSON.stringify(res, null, 2));
 
-    for (var i = 0; i < res.league.standard.length; i++){
-        console.log(res.league.standard[i].fullName)
+    raw_data = [];
+    for (var i = 0; i < res.league.standard.length; i++) {
+        if (res.league.standard[i].isNBAFranchise == true) {
+            var dict = {
+                teamName: res.league.standard[i].fullName,
+                teamId: res.league.standard[i].teamId
+            }
+        }
+        raw_data.push(dict);
     }
-})
+    let teams = [...new Set(raw_data)];
+    console.log(teams);
+}).catch(err => console.error(err));
