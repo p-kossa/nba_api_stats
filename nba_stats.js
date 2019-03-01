ba2464563c172bbd51111a3e3d278ed94298f6f0
Kevin Ossa <kossa526+git@gmail.com>
2019-03-01 12:18:25 -0500

    Code added for league standings

--- nba_stats.js
+++ nba_stats.js
@@ -1,9 +1,9 @@
 const nba = require('nba.js').default;
-
 var fs = require('fs');
+var get_teams, get_standings;
 
 // gets a list of dicts of team names and team ids
-var get_teams =  nba.data.teams({ year: '2018' }).then(res => {
+get_teams = nba.data.teams({ year: '2018' }).then(res => {
     fs.writeFileSync('teams.json', JSON.stringify(res, null, 2));
 
     raw_data = [];
@@ -18,20 +18,26 @@ var get_teams =  nba.data.teams({ year: '2018' }).then(res => {
     }
     let teams = [...new Set(raw_data)];
     return teams;
-
 }).catch(err => console.error(err));
 
 get_teams.then(function(result) {
     console.log(result)
 })
 
-var get_standings = nba.data.standings({ date: 'current' }).then(res => {
+get_standings = nba.data.standings({ date: 'current' }).then(res => {
     fs.writeFileSync('standings.json', JSON.stringify(res, null, 2));
 
+    raw_data = [];
     for (var i = 0; i < res.league.standard.teams.length; i++) {
-        
+        var dict = {
+            teamId: res.league.standard.teams[i].teamId,
+            win: res.league.standard.teams[i].win,
+            loss: res.league.standard.teams[i].loss
+        }
+        raw_data.push(dict);
     }
-})
+    return raw_data;
+}).catch(err => console.error(err));
 
 get_standings.then(function(result) {
     console.log(result);
